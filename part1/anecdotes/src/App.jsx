d710b2ad5bf16b88daa66f3994d6287d384bb875
AntonBragar <anton.bragar@gmail.com>
2023-08-24 06:35:05 +0300

    part 1 anecdotes done

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,43 @@
 import {useState} from 'react'
 
+const Header = ({text}) => <h2>{text}</h2>
+
+const Anecdote = ({anecdote, votes}) => {
+    return(
+        <div>
+            <p>{anecdote}</p>
+            <p>{votes}</p>
+        </div>
+    )
+}
+
+const Button = ({onClick, text}) => {
+    return(
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+const Winner = ({anecdotes, votes}) => {
+    const highestVoteCount = Math.max(...votes)
+    const winnerIndex = votes.indexOf(highestVoteCount)
+    const winner = anecdotes[winnerIndex]
+
+    if (highestVoteCount === 0) {
+        return (
+            <p>No votes yet</p>
+        )
+    }
+
+    return (
+        <div>
+            <p>{winner}</p>
+            <p>has {highestVoteCount} votes</p>
+        </div>
+    )
+}
+
 function App() {
     const anecdotes = [
         'If it hurts, do it more often.',
@@ -13,16 +51,27 @@ function App() {
     ]
 
     const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
     const getRandomNumber = () => {
         const randomNum = Math.floor(Math.random() * anecdotes.length)
         setSelected(randomNum)
     }
 
+    const handleVoteClick = () => {
+        const newAllVotes = [...votes]
+        newAllVotes[selected] += 1
+        setVotes(newAllVotes)
+    }
+
     return (
         <div>
-            <button onClick={getRandomNumber}>Get Anecdote</button>
-            {anecdotes[selected]}
+            <Header text="Anecdote of the day"/>
+            <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]}/>
+            <Button onClick={getRandomNumber} text="next anecdote"/>
+            <Button onClick={handleVoteClick} text="vote"/>
+            <Header text="Anecdote with most votes" />
+            <Winner anecdotes={anecdotes} votes={votes}/>
         </div>
     )
 }
