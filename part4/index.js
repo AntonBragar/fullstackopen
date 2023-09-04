58010904931abf42947b96094118d165ae0a3eee
AntonBragar <anton.bragar@gmail.com>
2023-09-04 12:57:34 -0400

    part4 step:1-2

--- part4/index.js
+++ part4/index.js
@@ -1,42 +1,35 @@
+require('dotenv').config()
+
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
+mongoose.set('strictQuery', false)
 
-const Blog = mongoose.model('Blog', blogSchema)
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require("./controllers/BlogRouter");
+const middleware = require('./utils/middleware')
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(express.json())
+app.use(cors())
+// app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
+morgan.token('content', (request) =>
+	request.method === 'POST' && request.body.title
+		? JSON.stringify(request.body)
+		: null
+)
 
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
+// app.use(middleware.unknownEndpoint)
+// app.use(middleware.errorHandler)
 
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+	logger.info(`Server running on port ${PORT}`)
 })
---
+++ part4/models/Blog.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
+
+const url = config.MONGODB_URI
+
+mongoose.connect(url)
+    .then(() => {
+        logger.info(`Connected to MongoDB`)
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+blogSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Blog', blogSchema)
